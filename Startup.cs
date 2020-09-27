21f7296667c23a7a4a11c68289bc7628c231cfda
suatozcan <email@example.com>
2020-09-27 14:07:23 +0300

    Remove UseHttpRedirection.

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,7 @@ namespace FitnessApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
 
             app.UseRouting();
 
