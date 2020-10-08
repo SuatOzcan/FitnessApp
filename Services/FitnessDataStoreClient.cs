f44a326840a73ae005ff1c5d4f4d70de1e66c519
suatozcan <email@example.com>
2020-10-08 17:17:11 +0300

    Add FitnessDataStoreClient class.

---
+++ Services/FitnessDataStoreClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Services
+{
+    public class FitnessDataStoreClient
+    {
+        private readonly IHttpClientFactory _httpFactory;
+        public FitnessDataStoreClient(IHttpClientFactory factoryInstance)
+        {
+            _httpFactory = factoryInstance;
+        }
+    }
+}
